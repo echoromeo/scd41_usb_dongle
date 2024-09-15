cb7f72f41c2fd191ecd6b7044c6e9347d9a3b41e
echoromeo <felis.lynx@gmail.com>
2024-09-15 15:05:14 +0200

    Initial DU project

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * avrdu.c
+ *
+ * Created: 15-Sep-24 15:03:07
+ * Author : egil
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
